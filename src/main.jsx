804155b1803315edc9e3d5cf152595d695421df0
Igor Grybanov <igor.gribanov@gmail.com>
2023-10-13 12:22:41 +0300

    settings

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/GooseTrack-with-backend-3.0">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
