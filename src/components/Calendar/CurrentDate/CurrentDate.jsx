0680831d270da46a2c2a3c0daed1006776f74f7c
Iryna Artymuk <artimyk@ukr.net>
2023-10-16 16:44:37 +0300

    fix button month back

--- src/components/Calendar/CurrentDate/CurrentDate.jsx
+++ src/components/Calendar/CurrentDate/CurrentDate.jsx
@@ -9,12 +9,12 @@ import {
 } from './CurrentDate.styled';
 import { useNavigate, useParams } from 'react-router-dom';
 const CurrentDate = ({ activePage, setActiveDate }) => {
-  // console.log('activePage: ', activePage);
+  console.log('activePage: ', activePage);
   const navigate = useNavigate();
   const params = useParams();
   const date = new Date(params.currentDate);
 
-  const getDatefromURL = () => {
+  const getDatefromURL = (date) => {
     if (Object.prototype.toString.call(date) === '[object Date]') {
       if (isNaN(date)) {
         return new Date();
@@ -24,17 +24,20 @@ const CurrentDate = ({ activePage, setActiveDate }) => {
     }
   };
 
-  const activeDate = getDatefromURL();
+  const activeDate = getDatefromURL(date);
 
-  const changeNextDate = (activeDate) => {
+  const changeNextDate = () => {
     activePage === 'month'
       ? navigate(`/calendar/month/${addMonths(activeDate, 1)}`)
       : navigate(`/calendar/day/${addDays(activeDate, 1)}`);
   };
-  const changePrevDate = (activePage) => {
+  const changePrevDate = () => {
+    const prevMonth = subMonths(activeDate, 1);
+    const prevDay = subDays(activeDate, 1);
+
     activePage === 'month'
-      ? navigate(`/calendar/month/${subMonths(activeDate, 1)}`)
-      : navigate(`/calendar/day/${subDays(activeDate, 1)}`);
+      ? navigate(`/calendar/month/${prevMonth}`)
+      : navigate(`/calendar/day/${prevDay}`);
   };
   return (
     <Controllers>
@@ -46,14 +49,10 @@ const CurrentDate = ({ activePage, setActiveDate }) => {
         <CurrentMonth> {format(activeDate, ' d MMM yyyy')}</CurrentMonth>
       </div>
       <div>
-        <Button
-          $direction="back"
-          $back
-          onClick={() => changePrevDate(activeDate)}
-        >
+        <Button $direction="back" $back onClick={() => changePrevDate()}>
           <StyledAiOutlineLeft />
         </Button>
-        <Button $direction="forward" onClick={() => changeNextDate(activeDate)}>
+        <Button $direction="forward" onClick={() => changeNextDate()}>
           <StyledAiOutlineRight />
         </Button>
       </div>
