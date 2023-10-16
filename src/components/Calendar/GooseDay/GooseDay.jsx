20604aa8af600fcee5fdc566ad3f5c7b3fb9be7a
Iryna Artymuk <artimyk@ukr.net>
2023-10-16 13:46:58 +0300

    add weekCalendar to GooseDay CalendarPage

--- src/components/Calendar/GooseDay/GooseDay.jsx
+++ src/components/Calendar/GooseDay/GooseDay.jsx
@@ -1,9 +1,10 @@
 import { useParams } from 'react-router-dom';
+import WeekCalendar from '../DayCalendarHead/WeekCalendar';
 
 export const GooseDay = () => {
   const params = useParams();
   const date = new Date(params.currentDate);
-  console.log('date : ', date );
-  // <  WeekCalendar/ >
-  return <div>goose Day</div>;
+  console.log('date : ', date);
+
+  return <WeekCalendar activeDate={date} />;
 };
