481eab0fc0dfd856b82610027e402622a413cba0
Igor Grybanov <igor.gribanov@gmail.com>
2023-10-18 17:10:06 +0300

    Routes

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,69 +1,69 @@
-// import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-// import {
-//   register,
-//   logIn,
-//   logOut,
-//   getCurrentUser,
-//   updateUser,
-//   getVerifyEmailUser,
-// } from './operations';
+import {
+    register,
+    logIn,
+    logOut,
+    getCurrentUser,
+    updateUser,
+    //   getVerifyEmailUser,
+} from './operations';
 
-// const initialState = {
-//   user: {
-//     _id: null,
-//     userName: null,
-//     email: null,
-//     avatarURL: null,
-//     phone: null,
-//     skype: null,
-//     birthDay: null,
-//     verify: null,
-//   },
-//   token: null,
-//   isLoggedIn: false,
-//   isFetchingCurrentUser: false,
-//   error: null,
-// };
+const initialState = {
+    user: {
+        userName: null,
+        _id: null,
+        email: null,
+        phone: null,
+        skype: null,
+        birthDay: null,
+        avatarURL: null,
+        verify: null,
+    },
+    token: null,
+    isLoggedIn: false,
+    isFetchingCurrentUser: false,
+    error: null,
+};
 
-// const authSlice = createSlice({
-//   name: 'auth',
-//   initialState,
-//   extraReducers: builder => {
-//     builder
-//       .addCase(register.fulfilled, (state, action) => {
-//         state.user = action.payload.user;
-//         state.token = action.payload.token;
-//         state.isLoggedIn = true;
-//       })
-//       .addCase(logIn.fulfilled, (state, action) => {
-//         state.user = action.payload.user;
-//         state.token = action.payload.token;
-//         state.isLoggedIn = true;
-//       })
-//       .addCase(logOut.fulfilled, state => {
-//         state.user = { name: null, email: null };
-//         state.token = null;
-//         state.isLoggedIn = false;
-//       })
-//       .addCase(getCurrentUser.pending, state => {
-//         state.isFetchingCurrentUser = true;
-//       })
-//       .addCase(getCurrentUser.fulfilled, (state, action) => {
-//         state.user = action.payload;
-//         state.isLoggedIn = true;
-//         state.isFetchingCurrentUser = false;
-//       })
-//       .addCase(getCurrentUser.rejected, state => {
-//         state.isFetchingCurrentUser = false;
-//       })
-//       .addCase(updateUser.fulfilled, (state, action) => {
-//         state.user = action.payload;
-//       })
-//       .addCase(getVerifyEmailUser.fulfilled, (state, action) => {
-//         state.user.verify = action.payload;
-//       });
-//   },
-// });
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    extraReducers: builder => {
+        builder
+            .addCase(register.fulfilled, (state, action) => {
+                state.user = action.payload.user;
+                state.token = action.payload.token;
+                state.isLoggedIn = true;
+            })
+            .addCase(logIn.fulfilled, (state, action) => {
+                state.user = action.payload.user;
+                state.token = action.payload.token;
+                state.isLoggedIn = true;
+            })
+            .addCase(logOut.fulfilled, state => {
+                state.user = { name: null, email: null };
+                state.token = null;
+                state.isLoggedIn = false;
+            })
+            .addCase(getCurrentUser.pending, state => {
+                state.isFetchingCurrentUser = true;
+            })
+            .addCase(getCurrentUser.fulfilled, (state, action) => {
+                state.user = action.payload;
+                state.isLoggedIn = true;
+                state.isFetchingCurrentUser = false;
+            })
+            .addCase(getCurrentUser.rejected, state => {
+                state.isFetchingCurrentUser = false;
+            })
+            .addCase(updateUser.fulfilled, (state, action) => {
+                state.user = action.payload;
+            })
+            // .addCase(getVerifyEmailUser.fulfilled, (state, action) => {
+            //     state.user.verify = action.payload;
+            // });
+    },
+});
 
-// export const authReducer = authSlice.reducer;
+export const authReducer = authSlice.reducer;
