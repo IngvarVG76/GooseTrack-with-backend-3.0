e701c2bd9ae9666a72b96220974c3a600caaef30
Serzh1006 <skravchenko223@gmail.com>
2023-10-19 12:54:23 +0200

    add styles statistics

--- src/components/statisticsChart/StatisticsChart.jsx
+++ src/components/statisticsChart/StatisticsChart.jsx
@@ -27,47 +27,80 @@ const data = [
   {
     name: 'To Do',
     day: 50,
-    month: 30,
+    month: 88,
   },
   {
     name: 'In Progress',
-    day: 25,
-    month: 10,
+    day: 90,
+    month: 80,
   },
   {
     name: 'Done',
-    day: 25,
-    month: 12,
+    day: 75,
+    month: 34,
   },
 ];
 
 const StatisticsChart = () => {
   return (
-    <div className={css.barChart}>
-      <h3 style={{ fontWeight: 600, fontSize: '14px', color: '#343434' }}>
-        Tasks
-      </h3>
+    <div className={css.chartContainer}>
+      <h3 className={css.tasksTitle}>Tasks</h3>
 
-      <BarChart width={355} height={360} data={data} barGap={9}>
+      <BarChart
+        className={css.barChart}
+        width={355}
+        height={360}
+        data={data}
+        barGap={8}
+      >
         <CartesianGrid vertical={false} fill="#fff" stroke="#E3F3FF" />
-
+        <defs>
+          <linearGradient
+            id="pinkGradientFill"
+            x1="0%"
+            x2="0%"
+            y1="0%"
+            y2="100%"
+          >
+            <stop offset="0%" stopColor="rgba(255, 210, 221, 0)" />
+            <stop offset="100%" stopColor="rgba(255, 210, 221, 1)" />
+          </linearGradient>
+          <linearGradient
+            id="blueGradientFill"
+            x1="0%"
+            x2="0%"
+            y1="0%"
+            y2="100%"
+          >
+            <stop offset="0%" stopColor="rgba(62, 133, 243, 0)" />
+            <stop offset="100%" stopColor="rgba(62, 133, 243, 1)" />
+          </linearGradient>
+        </defs>
         <XAxis
           dataKey="name"
           axisLine={false}
           tick={{ fill: '#343434', fontSize: '14px' }}
+          tickSize={20}
+          tickLine={false}
         />
         <YAxis
           type="number"
           domain={[0, 100]}
           axisLine={false}
-          width={27}
+          width={28}
           tickCount={6}
-          tickSize={0}
+          tickSize={10}
+          tickLine={false}
           tick={{ fill: '#343434', fontSize: '14px' }}
         />
         <Tooltip cursor={false} />
 
-        <Bar dataKey="day" barSize={22} className={css.barDay}>
+        <Bar
+          dataKey="day"
+          barSize={22}
+          fill="url(#pinkGradientFill)"
+          radius={6}
+        >
           <LabelList
             dataKey="day"
             position="top"
@@ -75,7 +108,12 @@ const StatisticsChart = () => {
             fontSize={12}
           />
         </Bar>
-        <Bar dataKey="month" barSize={22} className={css.barMonth}>
+        <Bar
+          dataKey="month"
+          barSize={22}
+          fill="url(#blueGradientFill)"
+          radius={6}
+        >
           <LabelList
             dataKey="month"
             position="top"
