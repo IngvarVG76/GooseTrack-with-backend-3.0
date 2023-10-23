fe7d57ae529fa02a23e92d9a484427727ba84860
Igor Grybanov <igor.gribanov@gmail.com>
2023-10-23 15:58:19 +0300

    tasks upd

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -3,20 +3,27 @@ import { Container } from './CalendarPage.styled';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import CalendarToolbar from '../../components/Calendar/CalendarToolbar/CalendarToolbar';
 import { format } from 'date-fns';
+import { fetchTasks } from '../../redux/tasks/task';
+import { useDispatch } from 'react-redux';
 
 const CalendarPage = () => {
   const [isActivePage] = useState(false);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const location = useLocation();
 
   useEffect(() => {
     const activeDate = format(new Date(), 'MMMM-yyyy');
-    if (location.pathname === '/calendar') {
+      if (location.pathname === '/calendar') {
       navigate(`/calendar/month/${activeDate}`);
-      return;
+         return;
     }
   }, [navigate, location.pathname]);
+
+    useEffect(() => {
+      const activeMonth = format(new Date(), 'yyyy-MM');
+      dispatch(fetchTasks(activeMonth));
+    }, [dispatch]);
 
   return (
     <Container>
