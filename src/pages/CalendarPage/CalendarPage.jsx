3340522e87d48b92326a8f2294610a3cfd6b09d3
OlegatorIT <podlesnyoleg93@gmail.com>
2023-10-23 15:39:55 +0200

    temp

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -4,26 +4,31 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import CalendarToolbar from '../../components/Calendar/CalendarToolbar/CalendarToolbar';
 import { format } from 'date-fns';
 import { fetchTasks } from '../../redux/tasks/task';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { GetDatefromURL } from '../../heplers/getDatefromURL';
 
 const CalendarPage = () => {
   const [isActivePage] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-
+  console.log(location.pathname);
+  const tasks = useSelector((state) => state);
+  console.log(tasks);
+  const test = GetDatefromURL();
+  console.log(test);
   useEffect(() => {
     const activeDate = format(new Date(), 'MMMM-yyyy');
-      if (location.pathname === '/calendar') {
+    if (location.pathname === '/calendar') {
       navigate(`/calendar/month/${activeDate}`);
-         return;
+      return;
     }
   }, [navigate, location.pathname]);
 
-    useEffect(() => {
-      const activeMonth = format(new Date(), 'yyyy-MM');
-      dispatch(fetchTasks(activeMonth));
-    }, [dispatch]);
+  useEffect(() => {
+    const activeMonth = format(new Date(), 'yyyy-MM');
+    dispatch(fetchTasks(activeMonth));
+  }, [dispatch]);
 
   return (
     <Container>
--- src/redux/tasks/handlers.js
+++ src/redux/tasks/handlers.js
@@ -1,6 +1,6 @@
 import { Notify } from 'notiflix';
 
-export const handlePending = state => {
+export const handlePending = (state) => {
   state.isLoading = true;
   state.error = null;
 };
@@ -13,18 +13,12 @@ export const handleRejected = (state, { payload }) => {
   state.error = payload;
 };
 
-export const handleFulfilled = (state, { payload }) => {
-  state.tasks = payload.tasks;
-  state.isLoading = false;
-  state.error = null;
-};
-
 export const handleMonthFulfilled = (state, { payload }) => {
-  state.tasks = payload.tasks;
-  if (payload.tasks.length === 0)
-  Notify.info('There are no tasks for this date.', {
-    timeout: 3000,
-  });
+  state.tasks = payload.data;
+  if (payload.data.length === 0)
+    Notify.info('There are no tasks for this date.', {
+      timeout: 3000,
+    });
   state.isLoading = false;
   state.error = null;
 };
