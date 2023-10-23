bd35bbb3caf7ccae365d6277f77e4dab184bd6dd
Serzh1006 <skravchenko223@gmail.com>
2023-10-23 17:33:15 +0200

    delete library

--- src/components/statisticsChart/StatisticsChart.jsx
+++ src/components/statisticsChart/StatisticsChart.jsx
@@ -6,16 +6,10 @@ import {
   CartesianGrid,
   Tooltip,
   LabelList,
-  ResponsiveContainer,
 } from 'recharts';
 import css from './chart.module.css';
 import { ChartContainer, Taskstitle } from './chartStyles';
 import { useTheme } from 'styled-components';
-
-// allTasksByDay=todoByDay+inprogressByDay+doneByDay
-// todoByDay%=todoByDay/allTasksByDay
-// inprogressByDay%=inprogressByDay/allTasksByDay
-// doneByDay%=doneByDay/allTasksByDay
 
 const statisticsData = [
   {
@@ -46,102 +40,107 @@ const data = [
 
 const StatisticsChart = () => {
   const theme = useTheme();
+  const formatValue = (value) => {
+    return !isNaN(value) && value !== 0 ? `${value.toFixed(0)}%` : '';
+  };
 
   return (
     <ChartContainer>
       <Taskstitle>Tasks</Taskstitle>
-      <ResponsiveContainer width={307} height={360}>
-        <BarChart
-          className={css.barChart}
-          width={307}
-          height={360}
-          data={data}
-          barGap={8}
-        >
-          <CartesianGrid
-            vertical={false}
-            fill={theme.colors.secondaryBackground}
-            stroke={theme.colors.borderColor}
-          />
-          <defs>
-            <linearGradient
-              id="pinkGradientFill"
-              x1="0%"
-              x2="0%"
-              y1="0%"
-              y2="100%"
-            >
-              <stop offset="0%" stopColor="rgba(255, 210, 221, 0)" />
-              <stop offset="100%" stopColor="rgba(255, 210, 221, 1)" />
-            </linearGradient>
-            <linearGradient
-              id="blueGradientFill"
-              x1="0%"
-              x2="0%"
-              y1="0%"
-              y2="100%"
-            >
-              <stop offset="0%" stopColor="rgba(62, 133, 243, 0)" />
-              <stop offset="100%" stopColor="rgba(62, 133, 243, 1)" />
-            </linearGradient>
-          </defs>
-          <XAxis
-            dataKey="name"
-            axisLine={false}
-            tick={{
-              fill: theme.colors.textColorTasksChart,
-              fontSize: '12px',
-              fontFamily: 'Inter',
-              fontWeight: 400,
-              lineHeight: 1.33,
-            }}
-            tickSize={20}
-            tickLine={false}
-          />
-          <YAxis
-            type="number"
-            domain={[0, 100]}
-            axisLine={false}
-            width={28}
-            tickCount={6}
-            tickSize={10}
-            tickLine={false}
-            tick={{
-              fill: theme.colors.textColorTasksChart,
-              fontSize: '14px',
-              fontFamily: 'Inter',
-            }}
-          />
-          <Tooltip cursor={false} />
+      <BarChart
+        className={css.barChart}
+        width={307}
+        height={360}
+        data={data}
+        barGap={8}
+      >
+        <CartesianGrid
+          vertical={false}
+          fill={theme.colors.secondaryBackground}
+          stroke={theme.colors.borderColor}
+        />
+        <defs>
+          <linearGradient
+            id="pinkGradientFill"
+            x1="0%"
+            x2="0%"
+            y1="0%"
+            y2="100%"
+          >
+            <stop offset="0%" stopColor="rgba(255, 210, 221, 0)" />
+            <stop offset="100%" stopColor="rgba(255, 210, 221, 1)" />
+          </linearGradient>
+          <linearGradient
+            id="blueGradientFill"
+            x1="0%"
+            x2="0%"
+            y1="0%"
+            y2="100%"
+          >
+            <stop offset="0%" stopColor="rgba(62, 133, 243, 0)" />
+            <stop offset="100%" stopColor="rgba(62, 133, 243, 1)" />
+          </linearGradient>
+        </defs>
+        <XAxis
+          dataKey="name"
+          axisLine={false}
+          tick={{
+            fill: theme.colors.textColorTasksChart,
+            fontSize: '12px',
+            fontFamily: 'Inter',
+            fontWeight: 400,
+            lineHeight: 1.33,
+          }}
+          tickSize={20}
+          tickLine={false}
+        />
+        <YAxis
+          type="number"
+          domain={[0, 100]}
+          axisLine={false}
+          width={28}
+          tickCount={6}
+          tickSize={10}
+          tickLine={false}
+          tick={{
+            fill: theme.colors.textColorTasksChart,
+            fontSize: '14px',
+            fontFamily: 'Inter',
+          }}
+        />
+        <Tooltip cursor={false} />
 
-          <Bar
-            dataKey="day"
-            barSize={22}
-            fill="url(#pinkGradientFill)"
-            radius={6}
-          >
-            <LabelList
-              dataKey="day"
-              position="top"
-              fill={theme.colors.textColorTasksChart}
-              fontSize={12}
-            />
-          </Bar>
-          <Bar
-            dataKey="month"
-            barSize={22}
-            fill="url(#blueGradientFill)"
-            radius={6}
-          >
-            <LabelList
-              dataKey="month"
-              position="top"
-              fill={theme.colors.textColorTasksChart}
-              fontSize={12}
-            />
-          </Bar>
-        </BarChart>
-      </ResponsiveContainer>
+        <Bar
+          dataKey="day"
+          barSize={22}
+          fill="url(#pinkGradientFill)"
+          radius={6}
+        >
+          <LabelList
+            dataKey="dayPercentage"
+            position="top"
+            offset={8}
+            fill={theme.colors.textColorTasksChart}
+            fontSize={12}
+            // formatter={formatValue(50)}
+          />
+        </Bar>
+        <Bar
+          dataKey="month"
+          barSize={22}
+          fill="url(#blueGradientFill)"
+          radius={6}
+        >
+          <LabelList
+            dataKey="monthPercentage"
+            position="top"
+            offset={8}
+            fill={theme.colors.textColorTasksChart}
+            fontSize={12}
+            // formatter={formatValue(90)}
+          />
+        </Bar>
+      </BarChart>
     </ChartContainer>
   );
 };
