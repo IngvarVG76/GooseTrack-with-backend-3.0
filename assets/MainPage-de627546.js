17b0def2ad881c77ff9c808bc9ba37cb3aca0c8e
IngvarVG76 <106869716+IngvarVG76@users.noreply.github.com>
2023-10-26 12:41:35 +0000

    Deploying to gh-pages from @ IngvarVG76/GooseTrack-with-backend-3.0@cc86760d7e6001d1f4ab4a2f31b68b53bd1c20cc 🚀

--- assets/MainPage-de627546.js
+++ assets/MainPage-de627546.js
@@ -1,4 +1,4 @@
-import{s as w,L as zt,r as H,j as d,c as ie,g as fn,a as pn,b as hn,M as gn}from"./index-6e96b2dd.js";import{L as vn,R as mn}from"./index.esm-bf2d849e.js";import{c as me}from"./index-09366e33.js";const yn=w.img`
+import{s as w,L as zt,r as H,j as d,c as ie,g as fn,a as pn,b as hn,M as gn}from"./index-34da5d8c.js";import{L as vn,R as mn}from"./index.esm-6a77a091.js";import{c as me}from"./index-02df88a1.js";const yn=w.img`
   width: 150px;
   height: 149px;
   background-size: cover;
